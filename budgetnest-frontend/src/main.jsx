63e9dc680c8e0e9bd038c6f98cd5069b2167ef0d
ramisettiBhargava <btej761@gmail.com>
2026-07-18 12:22:51 +0530

    Create main.jsx

---
+++ budgetnest-frontend/src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App.jsx";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "admin-lte/dist/css/adminlte.min.css";
+import "aos/dist/aos.css";
+import "./index.css";
+import "./styles/dashboard.css";
+import "./styles/sidebar.css";
+import "./styles/navbar.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
